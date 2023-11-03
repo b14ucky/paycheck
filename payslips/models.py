060e07acaef1a9eac989740359a22aebdccdbd80
Dominik <meisnerd2003@gmail.com>
2023-11-03 17:42:30 +0100

    feat: add method of getting payslips from database

---
+++ payslips/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Payslip(models.Model):
+    netPay = models.FloatField(null=False)
+    grossPay = models.FloatField(null=False)
+    taxPrepayment = models.FloatField(null=False)
+    socialInsuranceContribution = models.FloatField(null=False)
+    healtInsuranceContribution = models.FloatField(null=False)
+    dateOfPreparation = models.DateField(default=timezone.now)
