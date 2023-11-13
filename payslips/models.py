dde6680013a02badd252d387eeda1320abcca642
Dominik <meisnerd2003@gmail.com>
2023-11-13 21:06:02 +0100

    feat: improve payslip create and get methods

--- payslips/models.py
+++ payslips/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 class Payslip(models.Model):
+    employeeId = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     netPay = models.FloatField(null=False)
     grossPay = models.FloatField(null=False)
-    taxPrepayment = models.FloatField(null=False)
+    taxBasis = models.FloatField(null=False)
+    taxDue = models.FloatField(null=False)
+    taxPrepayment = models.IntegerField(null=False)
+    pensionContribution = models.FloatField(null=False)
+    disabilityContribution = models.FloatField(null=False)
+    sicknessInsuranceContribution = models.FloatField(null=False)
     socialInsuranceContribution = models.FloatField(null=False)
+    healthInsuranceContributionBasis = models.FloatField(null=False)
     healthInsuranceContribution = models.FloatField(null=False)
+    costsOfGettingIncome = models.IntegerField(null=False)
     dateOfPreparation = models.DateField(default=timezone.now)
--- payslips/serializers.py
+++ payslips/serializers.py
@@ -4,15 +4,15 @@ from .models import Payslip
 class PayslipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payslip
-        fields = ('id', 'netPay', 'grossPay', 'taxPrepayment', 'socialInsuranceContribution', 'healthInsuranceContribution', 'dateOfPreparation')
+        fields = ('id', 'employeeId', 'netPay', 'grossPay', 'taxBasis', 'taxDue', 'taxPrepayment', 'pensionContribution', 'disabilityContribution', 'sicknessInsuranceContribution', 'socialInsuranceContribution', 'healthInsuranceContributionBasis', 'healthInsuranceContribution', 'costsOfGettingIncome', 'dateOfPreparation')
 
 
 class CreatePayslipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payslip
-        fields = ('netPay', 'grossPay', 'taxPrepayment', 'socialInsuranceContribution', 'healthInsuranceContribution', 'dateOfPreparation')
+        fields = ('netPay', 'grossPay', 'taxBasis', 'taxDue', 'taxPrepayment', 'pensionContribution', 'disabilityContribution', 'sicknessInsuranceContribution', 'socialInsuranceContribution', 'healthInsuranceContributionBasis', 'healthInsuranceContribution', 'costsOfGettingIncome')
 
-    def create(self, data):
-        payslip = Payslip.objects.create(netPay=data['netPay'], grossPay=data['grossPay'], taxPrepayment=data['taxPrepayment'], socialInsuranceContribution=data['socialInsuranceContribution'], healthInsuranceContribution=data['healthInsuranceContribution'])
+    def create(self, data, user):
+        payslip = Payslip.objects.create(employeeId=user, netPay=data['netPay'], grossPay=data['grossPay'], taxBasis=data['taxBasis'], taxDue=data['taxDue'], taxPrepayment=data['taxPrepayment'], pensionContribution=data['pensionContribution'], disabilityContribution=data['disabilityContribution'], sicknessInsuranceContribution=data['sicknessInsuranceContribution'], socialInsuranceContribution=data['socialInsuranceContribution'], healthInsuranceContributionBasis=data['healthInsuranceContributionBasis'], healthInsuranceContribution=data['healthInsuranceContribution'], costsOfGettingIncome=data['costsOfGettingIncome'])
         payslip.save()
         return payslip
