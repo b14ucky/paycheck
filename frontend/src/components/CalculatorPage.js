c20c2ef9118dd44105866031f647aa13790b7033
Dominik <meisnerd2003@gmail.com>
2023-11-14 21:05:47 +0100

    feat: add prompt that confirms if payslip was created

--- frontend/src/components/CalculatorPage.js
+++ frontend/src/components/CalculatorPage.js
@@ -16,6 +16,7 @@ export default function CalculatorPage() {
 
     const [firstName, setFirstName] = useState();
     const [lastName, setLastName] = useState();
+    const [promptVisible, setPromptVisible] = useState(false);
 
     const numberOfHours = useRef(null);
     const hourlyWage = useRef(null);
@@ -52,6 +53,7 @@ export default function CalculatorPage() {
             }
         ).then(response => {
             event.target.reset();
+            setPromptVisible(true);
         })
         .catch(error => console.log(error))
     }
@@ -147,6 +149,22 @@ export default function CalculatorPage() {
                     </form>
                 </div>
             </section>
+            <div className="blurBackground" style={{visibility: promptVisible ? 'visible' : 'hidden'}}>
+                <div className="confirmationPromptContainer">
+                    <div className="confirmationPrompt">
+                        <div className="closeWrapper" onClick={() => setPromptVisible(false)}>
+                            <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#F6F6F3"><g id="SVGRepoBgCarrier" strokeWidth="0"></g><g id="SVGRepoTracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepoIconCarrier"> <g id="Menu / CloseLG"> <path id="Vector" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke="#F6F6F3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g> </g></svg>
+                        </div>
+                        <svg fill="#F6F6F3" width="64px" height="64px" viewBox="0 0 32.00 32.00" enableBackground="new 0 0 32 32" version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" stroke="#F6F6F3" strokeWidth="0.00032"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepoTracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.064"></g><g id="SVGRepoIconCarrier"> <g id="Approved"> <g> <path d="M16,1C7.729,1,1,7.729,1,16s6.729,15,15,15s15-6.729,15-15S24.271,1,16,1z M16,29C8.832,29,3,23.168,3,16S8.832,3,16,3 s13,5.832,13,13S23.168,29,16,29z"></path> <path d="M23.317,10.27l-10.004,9.36l-4.629-4.332c-0.403-0.377-1.035-0.356-1.413,0.047c-0.377,0.403-0.356,1.036,0.047,1.413 l5.313,4.971c0.192,0.18,0.438,0.27,0.683,0.27s0.491-0.09,0.683-0.27l10.688-10c0.403-0.377,0.424-1.01,0.047-1.413 C24.353,9.913,23.719,9.892,23.317,10.27z"></path> </g> </g> <g id="Approved_1_"></g> <g id="FileApprove"></g> <g id="FolderApproved"></g> <g id="SecurityApproved"></g> <g id="CertificateApproved"></g> <g id="UserApproved"></g> <g id="IDCardApproved"></g> <g id="AndroidApproved"></g> <g id="PrivacyApproved"></g> <g id="Approved_2_"></g> <g id="MessageApproved"></g> <g id="UploadApproved"></g> <g id="DownloadApproved"></g> <g id="EmailApproved"></g> <g id="DataApproved"></g> </g></svg>
+                        <br />
+                        <br />
+                        <a className="promptTitle">Payslip Successfully Created</a>
+                        <br />
+                        <br />
+                        <input className="OnFocus button buttonAnimation" type="button" id="downloadPDFButton" value="Download PDF" />
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
