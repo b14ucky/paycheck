9b37b217db91353b94b646f6dbe57d1c8c618810
Dominik <101473446+b14ucky@users.noreply.github.com>
2023-12-10 18:01:37 +0100

    chore: update settings.py

--- payslip_generator/settings.py
+++ payslip_generator/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-sm+rh8s@dwvw7!s=lqb*7%$!tg&y*+d3w5v#7l+f1-cs_q9wf7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
